5473d109d42325bb9af584083e7c75e356fae1bd
ajf109 <70493841+ajf109@users.noreply.github.com>
2021-10-19 16:16:55 -0400

    request accept/decline
    admin can now see all current host role requests and can accept or decline them

--- src/App.js
+++ src/App.js
@@ -65,7 +65,8 @@ function App() {
     //create the request document in firestore
     db.collection("requests").doc(uid).set({
       name: name,
-      reason: reason
+      reason: reason,
+      user_id: uid
     })
     .then(() => {
       console.log("Document successfully written!");
@@ -115,6 +116,20 @@ function App() {
 function SignInScreen() {
   const [isSignedIn, setIsSignedIn] = useState(false); // Local signed-in state.
   const [userRole, setUserRole] = useState();  // Local user role state
+  const [requests, setRequests] = useState([]); // List of requests
+
+  const ref = firebase.firestore().collection("requests");
+
+  //put request entries from database in the "requests" local state
+  function getRequests() {
+    ref.onSnapshot((querySnapshot) => {
+      const items = [];
+      querySnapshot.forEach((doc) => {
+        items.push(doc.data());
+      });
+      setRequests(items);
+    })
+  }
 
   //checks to see if given user is in the database, if not, adds them as a student
   const checkForNewUser = async (uid) =>
@@ -146,6 +161,30 @@ function SignInScreen() {
     }
   }
 
+  //called when admin accepts a host role request
+  //deletes the request and sets the requesting user's role to host
+  function acceptRequest(uid)
+  {
+    db.collection("users").doc(uid).set({
+      role: "host"
+    })
+    .then(() => {
+      console.log("Document successfully written!");
+    })
+    .catch((error) => {
+      console.error("Error writing document: ", error);
+    });
+
+    db.collection("requests").doc(uid).delete();
+  }
+
+  //called when admin declines a host role request
+  //deletes the request without changing the user's role
+  function declineRequest(uid)
+  {
+    db.collection("requests").doc(uid).delete();
+  }
+
   // Listen to the Firebase Auth state and set the local state.
   useEffect(() => {
     const unregisterAuthObserver = firebase.auth().onAuthStateChanged(user => {
@@ -162,6 +201,10 @@ function SignInScreen() {
     return () => unregisterAuthObserver(); // Make sure we un-register Firebase observers when the component unmounts.
   }, []);
 
+  useEffect(() => {
+    getRequests();
+  }, []);
+
   if (!isSignedIn) {
     return (
       <div>
@@ -176,7 +219,7 @@ function SignInScreen() {
   {
     return (
       <div>
-        <h1>Student</h1>
+        <h1>Student Homepage</h1>
         <Link to="/request_form">Request Access</Link>
         <br></br>
         <button className="sign-out" onClick={() => firebase.auth().signOut()}>Sign-out</button>
@@ -188,7 +231,7 @@ function SignInScreen() {
   {
     return (
       <div>
-        <h1>Host</h1>
+        <h1>Host Homepage</h1>
         <button className="sign-out" onClick={() => firebase.auth().signOut()}>Sign-out</button>
       </div>
     );
@@ -198,7 +241,16 @@ function SignInScreen() {
   {
     return (
       <div>
-        <h1>Admin</h1>
+        <h1>Admin Homepage</h1>
+        {requests.map((request) => (
+          <div key = {request.user_id}>
+            <div>{request.name}</div> <br></br>
+            <div>{request.reason}</div> <br></br>
+            <button onClick={() => acceptRequest(request.user_id)}>Accept</button>
+            <button onClick={() => declineRequest(request.user_id)}>Decline</button>
+            <br></br><br></br><br></br><br></br>
+          </div>
+        ))}
         <button className="sign-out" onClick={() => firebase.auth().signOut()}>Sign-out</button>
       </div>
     );
