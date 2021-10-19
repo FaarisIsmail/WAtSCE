a0195f8a56422b0a108faeefee3b24de8c385d28
ajf109 <70493841+ajf109@users.noreply.github.com>
2021-10-18 20:24:03 -0400

    routing/backend role change requests
    Testing out routing, having different homepages for the three roles (admin, host, student), and being able to request the host role.

--- src/App.js
+++ src/App.js
@@ -8,8 +8,11 @@ import 'firebase/compat/firestore';
 import StyledFirebaseAuth from 'react-firebaseui/dist/StyledFirebaseAuth';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
-
-
+import { getModularInstance } from '@firebase/util';
+import { doc, getDoc } from 'firebase/firestore';
+import { withRouter } from "react-router-dom";
+import { BrowserRouter as Router, Link, useHistory, Redirect} from "react-router-dom";
+import Route from "react-router-dom/Route";
 
 firebase.initializeApp({
   apiKey: "AIzaSyDny8PAsKPiFPsdp8ml7M4xlu-XLwsNjgM",
@@ -23,6 +26,8 @@ firebase.initializeApp({
 
 const auth = firebase.auth();
 const firestore = firebase.firestore();
+const db = firebase.firestore();
+
 
 const uiConfig = {
   // Popup signin flow rather than redirect flow.
@@ -40,28 +45,119 @@ const uiConfig = {
   },
 };
 
-
 function App() {
-
-  
   const [user] = useAuthState(auth);
+  const history = useHistory();
+
+  //function called when request form is submitted
+  //creates a new request document w/ the data from the request form
+  //entry will be named after the user id (but i might change that later)
+  const RequestAccess = async (e) =>
+  {
+    //get info from request form
+    let name = e.target.rname.value;
+    let reason = e.target.reason.value;
+
+    //get the user id of the currently logged in user 
+    let user = firebase.auth().currentUser;
+    let uid = user.uid;
+
+    //create the request document in firestore
+    db.collection("requests").doc(uid).set({
+      name: name,
+      reason: reason
+    })
+    .then(() => {
+      console.log("Document successfully written!");
+    })
+    .catch((error) => {
+      console.error("Error writing document: ", error);
+    });
+
+    //go back to the homepage when finished
+    history.push('/');
+  }
 
   return (
     <div className="App">
-      <header className="App-header">
-        <SignInScreen />
-      </header>
+      <Route path="/" exact strict render={
+        () => {
+          return (
+            <header className="App-header">
+              <SignInScreen />
+            </header>
+          );
+        }
+      }/>
+      <Route path="/request_form" exact strict render={ //form to gather data for a host role request
+        () => {
+          return (
+            <div>
+              <form onSubmit={RequestAccess}>
+                <div>Enter your name:</div> <br></br>
+                <input type="text" id="rname" name="rname"></input> <br></br>
+                <div>Why do you want to be able to create events?</div> <br></br>
+                <input type="text" id="reason" name="reason"></input> <br></br>
+                <button>Submit</button>
+              </form>
+            </div>
+          );
+        }
+      }/>
+      
     </div>
+    
+    
+    
   );
 }
 
 function SignInScreen() {
   const [isSignedIn, setIsSignedIn] = useState(false); // Local signed-in state.
+  const [userRole, setUserRole] = useState();  // Local user role state
+
+  //checks to see if given user is in the database, if not, adds them as a student
+  const checkForNewUser = async (uid) =>
+  {
+    //get document from database for the currently logged in user
+    const docRef = doc(db, "users", uid);
+    const docSnap = await getDoc(docRef);
+
+    //user not in database yet, so create a new document
+    if (!docSnap.exists())
+    {
+      db.collection("users").doc(uid).set({
+        role: "student" //can be student, admin, or host
+      })
+      .then(() => {
+        console.log("Document successfully written!");
+      })
+      .catch((error) => {
+        console.error("Error writing document: ", error);
+      });
+
+      setUserRole("student");
+    }
+    else
+    {
+      // get the current user's role
+      let r = docSnap.get("role");
+      setUserRole(r);
+    }
+  }
 
   // Listen to the Firebase Auth state and set the local state.
   useEffect(() => {
     const unregisterAuthObserver = firebase.auth().onAuthStateChanged(user => {
       setIsSignedIn(!!user);
+      
+      if (user)
+      {
+        let uid = user.uid;
+
+        //check if the user is in the database when they log in
+        checkForNewUser(uid);
+      }
     });
     return () => unregisterAuthObserver(); // Make sure we un-register Firebase observers when the component unmounts.
   }, []);
@@ -75,13 +171,38 @@ function SignInScreen() {
       </div>
     );
   }
-  return (
-    <div>
-      <h1>My App</h1>
-      <p>Welcome {firebase.auth().currentUser.displayName}! You are now signed-in! </p>
-      <button className="sign-out" onClick={() => firebase.auth().signOut()}>Sign-out</button>
-    </div>
-  );
+  //student homepage
+  else if (userRole == "student")
+  {
+    return (
+      <div>
+        <h1>Student</h1>
+        <Link to="/request_form">Request Access</Link>
+        <br></br>
+        <button className="sign-out" onClick={() => firebase.auth().signOut()}>Sign-out</button>
+      </div>
+    );
+  }
+  //host homepage
+  else if (userRole == "host")
+  {
+    return (
+      <div>
+        <h1>Host</h1>
+        <button className="sign-out" onClick={() => firebase.auth().signOut()}>Sign-out</button>
+      </div>
+    );
+  }
+  //admin homepage
+  else
+  {
+    return (
+      <div>
+        <h1>Admin</h1>
+        <button className="sign-out" onClick={() => firebase.auth().signOut()}>Sign-out</button>
+      </div>
+    );
+  }
 }
 
 export default App;
