8c2eb422831746f2431b85f040dcafeb7989c456
ajf109 <70493841+ajf109@users.noreply.github.com>
2021-10-27 11:28:19 -0400

    Event creation/registration

--- src/Pages/CreateEvent.js
+++ src/Pages/CreateEvent.js
@@ -12,8 +12,11 @@ export default function CreateEvent() {
   const [isSignedIn, setIsSignedIn] = useState(false); // Local signed-in state.
   const [userRole, setUserRole] = useState();  // Local user role state
   const [requests, setRequests] = useState([]); // List of requests
+  const [events, setEvents] = useState([]); //List of events that the user is hosting
+
 
   const ref = firestore.collection("requests");
+  const eventRef = firestore.collection("events").where("host_id", "==", auth.currentUser.uid);
 
   //put request entries from database in the "requests" local state
   function getRequests() {
@@ -23,6 +26,17 @@ export default function CreateEvent() {
         items.push(doc.data());
       });
       setRequests(items);
+    })
+  }
+
+  //put event entries that the user is hosting into the "events" local state
+  function getEvents() {
+    eventRef.onSnapshot((querySnapshot) => {
+      const items = [];
+      querySnapshot.forEach((doc) => {
+        items.push(doc);
+      });
+      setEvents(items);
     })
   }
 
@@ -80,6 +94,45 @@ export default function CreateEvent() {
     db.collection("requests").doc(uid).delete();
   }
 
+  //called when host clicks the "create event" button,
+  //creates an event with the given data
+  const CreateEvent = async (e) =>
+    {
+      let name = e.target.event_name.value;
+      let description = e.target.description.value;
+  
+      //get the user id of the currently logged in user 
+      let user = auth.currentUser;
+      let uid = user.uid;
+  
+      //create the event document in firestore
+      db.collection("events").doc().set({
+        name: name,
+        description: description,
+        host_id: uid
+      })
+      .then(() => {
+        console.log("Document successfully written!");
+      })
+      .catch((error) => {
+        console.error("Error writing document: ", error);
+      });
+  
+      //go back to the homepage when finished
+      history.push('/');
+    }
+
+    //deletes the event and all associated user registrations
+    function deleteEvent(event_id)
+    {
+      db.collection("events").doc(event_id).delete();
+      
+      //TODO: delete all registrations where event_id = the event_id for the event which we are deleting
+
+      //apparantly it is better to delete subcollections using the Firebase CLI, but our number of users
+      //might be small enough for it to not matter
+    }
+
   // Listen to the Firebase Auth state and set the local state.
   useEffect(() => {
     const unregisterAuthObserver = auth.onAuthStateChanged(user => {
@@ -98,10 +151,11 @@ export default function CreateEvent() {
 
   useEffect(() => {
     getRequests();
+    getEvents();
   }, []);
 
   //student homepage
-  if (userRole == "student")
+  if (userRole === "student")
   {
     return (
       <div class='center'>
@@ -115,17 +169,40 @@ export default function CreateEvent() {
     );
   }
   //host homepage
-  else if (userRole == "host")
+  else if (userRole === "host")
   {
     return (
       <div>
         <h1>Host Homepage</h1>
+          <form onSubmit={CreateEvent}>
+            <div>Enter the event name:</div> <br></br>
+            <input type="text" id="event_name" name="event_name"></input> <br></br><br></br>
+            <div>Enter a description for the event:</div> <br></br>
+            <input type="text" id="description" name="description"></input> <br></br><br></br>
+            <div>Enter the event date:</div> <br></br>
+            <input type="date" id="date" name="date"></input><br></br><br></br>
+            <div>Start Time:</div> <br></br>
+            <input type="time" id="start" name="start"></input><br></br><br></br>
+            <div>End Time:</div> <br></br>
+            <input type="time" id="end" name="end"></input><br></br><br></br>
+            <button>Submit</button>
+          </form>
+          <br></br><br></br><br></br><br></br>
+          <h1>Your Events</h1>
+          {events.map((event) => (
+          <div key = {event.name}>
+            <div>{event.data().name}</div> <br></br>
+            <div>{event.data().description}</div> <br></br>
+            <button onClick={() => deleteEvent(event.id)}>Cancel</button>
+            <br></br><br></br><br></br><br></br>
+          </div>
+        ))}
         <button className="test" onClick={() => auth.signOut()}>Sign-out</button>
       </div>
     );
   }
   //admin homepage
-  else if (userRole == "admin")
+  else if (userRole === "admin")
   {
     return (
       <div>
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,106 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { doc, getDoc } from 'firebase/firestore';
+import { BrowserRouter as Router, Link, useHistory, Redirect} from "react-router-dom";
+import {auth, db, firestore} from '../firebase.js'
+import './CreateEvent.css';
+import { Button } from '../components/Button'
 
 export default function Home1() {
+  const history = useHistory();
+  const [events, setEvents] = useState([]); // List of all events
+  const [registrations, setRegistrations] = useState([]); //List of registrations for current user
+  const ref = firestore.collection("events").orderBy("name");
+  const registrationRef = firestore.collection("registrations").where("user_id", "==", auth.currentUser.uid);
+
+  //put event entries from database in the "events" local state
+  function getEvents() {
+    ref.onSnapshot((querySnapshot) => {
+      const items = [];
+      querySnapshot.forEach((doc) => {
+       // items.push(doc.data());
+        items.push(doc);
+      });
+      setEvents(items);
+    })
+  }
+
+  //put registration entries for the current user in the "registrations" local state
+  function getRegistrations() {
+    registrationRef.onSnapshot((querySnapshot) => {
+      const items = [];
+      querySnapshot.forEach((doc) => {
+       // items.push(doc.data());
+        items.push(doc);
+      });
+      setRegistrations(items);
+    })
+  }
+
+  //create a new registration entry for the given event id and current user id
+  //the id for this new registration entry is <eventId>_<userId>, and it contains
+  //the event and user id's as data fields
+  function registerForEvent(id)
+  {
+    db.collection("registrations").doc(id + "_" + auth.currentUser.uid).set({
+      user_id: auth.currentUser.uid,
+      event_id: id
+    })
+  }
+
+  //return true if the user is currently registered for the given event, and false if otherwise
+  function registrationExists(event_id)
+  {
+    let registration = registrations.find(r => r.id === event_id + "_" + auth.currentUser.uid);
+    if (registration == undefined)
+    {
+      return false;
+    }
+    else
+    {
+      return true;
+    }
+
+  }
+
+  //delete the given registration entry
+  function cancelRegistration(event_id)
+  {
+    db.collection("registrations").doc(event_id + "_" + auth.currentUser.uid).delete();
+  }
+
+  useEffect(() => {
+    getEvents();
+    getRegistrations();
+  }, []);
+
+  //returns a list of all events
+  //if the user is not registered for an event or is not the host, the "register" button will be shown
+  //if the user is registered for the event, the "cancel registration" button will be shown
+  //TODO: if the user is the host, the "cancel event" button will be shown
   return (
     <div>
-      This is the home page.
+      <h1>Events</h1>
+      {events.map((event) => (
+        <div key = {event.id}>
+          <div>{event.data().name}</div><br></br>
+          <div>{event.data().description}</div> <br></br>
+          {!registrationExists(event.id) && auth.currentUser.uid != event.data().host_id &&
+            <button onClick={() => registerForEvent(event.id)}>Register</button>
+          }
+          {registrationExists(event.id) &&
+            <button onClick={() => cancelRegistration(event.id)}>Cancel Registration</button>
+          }
+          <br></br><br></br><br></br><br></br>
+        </div>
+      ))}
+
+      <h1>My Schedule</h1>
+      {events.map((event) => (
+        <div key = {event.id}>
+          {registrationExists(event.id) &&
+          <><div>{event.data().name}</div><br></br><div>{event.data().description}</div><br></br><button onClick={() => cancelRegistration(event.id)}>Cancel Registration</button><br></br><br></br><br></br><br></br></>}
+        </div>
+      ))}
     </div>
   )
 }
