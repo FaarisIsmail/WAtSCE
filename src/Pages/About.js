5720046b5ebe8b60c808fd8a8545afbbed7b70eb
Avaex <jgf18@pitt.edu>
2021-10-27 21:13:33 -0400

    Added about page description, changed CSS (temp)

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import {auth} from '../firebase.js'
 
+import './About.css';
+
 export default function About() {
   return (
-    <div>
-      This is the about page.
+    <div className="about">
+      <h2> About WATSCE</h2>
+      <p>      
+      Welcome to WATSCE! This is a website created by Pitt students with the overarching goal of 
+      providing Hill District high school students with new opporunities to learn and apply digital skills. 
+      Teachers, local business owners, and community leaders are able to use WATSCE to create and post events
+      for students to attend. Once students arrive at an event, they can check in using QR scanner
+      technology, providing valuable feedback and data for event hosts. To start hosting events, simply sign in using
+      your mobile phone and request host access on our Create an Event page.  
+      </p>
     </div>
   )
 }
