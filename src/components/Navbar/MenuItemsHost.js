4a87280e69c3b613022f0a9215aa6de543a87a76
Avaex <jgf18@pitt.edu>
2021-12-04 23:19:33 -0500

    Different navbars for students/hosts

---
+++ src/components/Navbar/MenuItemsHost.js
@@ -0,0 +1,27 @@
+export const MenuItemsHost = [
+  {
+    title: 'Home',
+    url: '/',
+    cName: 'nav-links',
+  },
+  {
+    title: 'My Schedule',
+    url: '/schedule',
+    cName: 'nav-links',
+  },
+    {
+      title: 'Create an event',
+      url: '/create-event',
+      cName: 'nav-links',
+  },
+    {
+      title: 'My Created Events',
+      url: '/my-created-events',
+      cName: 'nav-links',
+  },
+    {
+    title: 'About',
+    url: '/about',
+    cName: 'nav-links',
+  },
+];
